4a9838094d7c5ba9751bfff865c2ca2e82641989
lizelive <git@lize.live>
2026-04-18 09:14:45 -0700

    purge attempt

--- src/domain/catalog.ts
+++
@@ -1,6 +0,0 @@
-import { oniEntities } from "../../data/oni/index.js";
-import { OniRegistry } from "./registry.js";
-
-export function createCatalog(): OniRegistry {
-  return new OniRegistry(oniEntities);
-}
--- src/optimize/z3-context.ts
+++
@@ -1,8 +0,0 @@
-import { init } from "z3-solver";
-
-let cachedPromise: ReturnType<typeof init> | undefined;
-
-export async function getZ3() {
-  cachedPromise ??= init();
-  return cachedPromise;
-}
